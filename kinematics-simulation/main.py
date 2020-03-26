9c4b32a84afc0d5284f73d0f954ac0c52efd0c76
Bill <billcshto@gmail.com>
2020-03-25 23:46:32 -0400

    kinematics-simulation: add grid

--- kinematics-simulation/main.py
+++ kinematics-simulation/main.py
@@ -3,7 +3,7 @@ import math
 
 from projectile import Projectile
 from grid import Grid
-from pygame_util_library import InputBox
+from pygame_util_library import InputBox, TextBox
 import window_info as windowInfo
 
 environment = {
@@ -18,14 +18,26 @@ def timeToGround( location, velocity, acceleration):
   root = math.sqrt(b**2 - 4 * a * c);
   return abs((-b - root) / (2 * a));
 
+def drawGrid():
+  gridGapLength = 30
+  #x-axis
+  pygame.draw.line(win, (0,0,150), (windowInfo.width/2, windowInfo.groundHeight),(windowInfo.width/2, windowInfo.inputHeight))
+  for i in range( windowInfo.inputHeight + gridGapLength, windowInfo.groundHeight, gridGapLength):
+    yLabel = TextBox(windowInfo.width/2 - gridGapLength, i, 5, 5, str((i - windowInfo.groundHeight) * -1), 5)
+    yLabel.draw(win)
+  #y-axis
+  pygame.draw.line(win, (0,0,0), (0, windowInfo.groundHeight),(windowInfo.width,windowInfo.groundHeight))
+  for i in range( gridGapLength, windowInfo.width - gridGapLength, gridGapLength):
+    xLabel = TextBox(i, windowInfo.groundHeight + 5, 5, 5, str(i), 5)
+    xLabel.draw(win)
+
 def drawGameWindow():
   win.fill((255,255,255))
   for i in range(len(location.y)):
     pygame.draw.rect(win, (0, 0, 0), (location.x[i], location.y[i] + projectile.width / 2, 1, 1))
-  instructions = font.render("click anywhere to reset projectile", 1, (0,0,0))
-  win.blit(instructions, (200, windowInfo.inputHeight + 20))
+  win.blit(instructions, (100, windowInfo.inputHeight + 20))
   pygame.draw.line(win, (0,0,0), (0, windowInfo.inputHeight),(windowInfo.width,windowInfo.inputHeight))
-  pygame.draw.line(win, (0,0,0), (0, windowInfo.groundHeight),(windowInfo.width,windowInfo.groundHeight))
+  drawGrid()
   pygame.draw.rect(win, (0, 0, 0), (location.getCurrentX(), location.getCurrentY(), projectile.width, projectile.width))
   velocityInput.draw(win)
   angleInput.draw(win)
@@ -35,10 +47,11 @@ def drawGameWindow():
 
 pygame.init()
 font = pygame.font.SysFont('arial', 15)
+instructions = font.render("click anywhere to reset projectile", 1, (0,0,0))
 
 #enviroment setup
 velocityInput = InputBox(50, 50, 50, 25, 'enter the velocity of the initial projectile in m/s:', 20, (220,220,220))
-angleInput = InputBox(windowInfo.width/2, 50, 50, 25, 'enter the degree of the initial projectile in degrees above the horizon: ', 20, (220,220,220))
+angleInput = InputBox(windowInfo.width/2 + 20, 50, 50, 25, 'enter the degree of the initial projectile in degrees above the horizon: ', 20, (220,220,220))
 projectile = Projectile(5)
 location = Grid()
 accumulatedTime = 0
